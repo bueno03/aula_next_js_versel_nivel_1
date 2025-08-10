79576eb3871ec3a2022f396d51fb197ea8cdeffa
Anderson Bueno Trindade <anderson.bueno@sap.com>
2025-08-10 02:13:09 -0300

    rollback variavel de ambiente Prod vs Homolog

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,9 @@
 import Link from 'next/link';
 
 function Home() {
-    let environmentType = "Nao definido";
-
-    if (process.env.ENVIRONMENT_TYPE == "PROD") {
-        environmentType = "PROD";
-    }
-
-    if (process.env.ENVIRONMENT_TYPE == "HOMOLOG") {
-        environmentType = "HOMOLOG";
-    }
 
     return (
         <div>
-            <h3>Ambiente:  {environmentType}</h3>
             <h1>Home</h1>
             <Link href="/sobre">
                 Pagina sobre
