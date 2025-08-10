27d1abb967661f707d5d1f87595a581178e2c1ff
Anderson Bueno Trindade <anderson.bueno@sap.com>
2025-08-09 23:01:45 -0300

    Link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import Link from 'next/link';
+
 function Home() {
-    return <div>Home Homologação</div>
+    return (
+        <div>
+            <h1>Home Homologação</h1>
+            <Link href="/sobre">
+                <a>Pagina sobre</a>
+            </Link>
+        </div>
+    )
 }
 
 export default Home
