f9d90f234012e32bb21ec170fa27b61238b9fa1c
Anderson Bueno Trindade <anderson.bueno@sap.com>
2025-08-10 01:34:02 -0300

    variavel de ambiente Prod vs Homolog

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,16 @@ import Link from 'next/link';
 function Home() {
     let environmentType = "Nao definido";
 
-    if (process.env.ENVIRONMENT_TYPE !== "") {
-        environmentType = process.env.ENVIRONMENT_TYPE;
+    if (process.env.ENVIRONMENT_TYPE == "PROD") {
+        environmentType = "PROD";
+    } else {
+        environmentType = "HOMOLOG";
     }
 
     return (
         <div>
-            <h1>Ambiente:</h1> {environmentType}
-            <h1>Home Homologação</h1>
+            <h3>Ambiente:  {environmentType}</h3>
+            <h1>Home</h1>
             <Link href="/sobre">
                 Pagina sobre
             </Link>
